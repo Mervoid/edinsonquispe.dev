48876b3c5682f2f0f1e949d304d4e52795264d0e
Unvird <77942446+Mervoid@users.noreply.github.com>
2025-10-30 18:53:27 -0500

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import ghPages from '@astrojs/gh-pages';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://TU_USUARIO.github.io',
+  base: '/edinsonquispe.dev',
+  adapter: ghPages()
+});
