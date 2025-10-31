dae0586a52f1dcecd5cc6e2b140233bb3f7e425c
Unvird <77942446+Mervoid@users.noreply.github.com>
2025-10-30 23:06:21 -0500

    13 - cambio numero 2 deploy 2do en el host

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://edinsonquispe.dev',
   base: '/',
   output: 'static',
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
